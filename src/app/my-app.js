7c34611f5b44dfd33c7716f7985002359727e7a1
sakito <sakito21@gmail.com>
2020-03-18 11:27:01 +0900

    add minimum environment

---
+++ src/app/my-app.js
@@ -0,0 +1,11 @@
+import { LitElement, html } from "lit-element";
+
+class MyApp extends LitElement {
+  render() {
+    return html`
+      <h1>Hello World</h1>
+    `;
+  }
+}
+
+customElements.define("my-app", MyApp);
